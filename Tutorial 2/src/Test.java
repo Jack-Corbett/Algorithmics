6e1c81b3e9c10eb3c4e47693d4ac39a62df6a9ce
Jack <jackcorbett1998@hotmail.co.uk>
2020-03-31 21:38:59 +0100

    Added tutorial 2

---
+++ Tutorial 2/src/Test.java
@@ -0,0 +1,18 @@
+import java.util.Iterator;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Ring<Integer> ring = new CircularArrayRing<>();
+
+        for(int i=0; i<1; ++i) {
+            ring.add(i);
+        }
+
+        Iterator<Integer> it = ring.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
